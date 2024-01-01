973522930a62436b770c9af99ab501f8e3249bc9
Леонид Половков <leonid.spb2015@yandex.ru>
2024-01-01 22:16:39 +0300

    Правка сайта

--- app/js/validate-form.js
+++ app/js/validate-form.js
@@ -5,6 +5,9 @@ let form = document.querySelector('.form'),
     // inputEmail = document.querySelector('.js-input-mail'),
     // inputCheckbox = document.querySelector('.js-input-checkbox'),
     inputTextArea = document.querySelector('.js-input-textarea');
+notifyName = document.querySelector('.notify-name');
+notifyPhone = document.querySelector('.notify-phone');
+notifyTextArea = document.querySelector('.notify-text-area');
 
 
 /*function validateEmail(email) {
@@ -19,16 +22,24 @@ function validateName(inputName) {
     return re.test(String(inputName).toLowerCase());
 }
 
-
 function validatePhone(inputPhone) {
     let re = /^((\+7|7|8)+([0-9]){10})$/i;
     return re.test(String(inputPhone).toLowerCase());
 }
+function validateTextArea(inputTextArea) {
+    let re = /^[a-zA-Zа-яёА-ЯЁ]+$/u;
+    return re.test(String(inputTextArea).toLowerCase());
+}
+
+
+
+
 
 
 form.onsubmit = function () {
     let phoneVal = inputPhone.value,
         nameVal = inputName.value,
+        textAreaVal = inputTextArea.value,
         emptyInputs = Array.from(formInputs).filter(input => input.value === '');
 
     formInputs.forEach(function (input) {
@@ -47,28 +58,31 @@ form.onsubmit = function () {
     }
 
     if (!validateName(nameVal)) {
-        console.log('Введите свое имя');
-        inputName.classList.add('.error');
+        inputName.classList.add('error');
+        notifyName.classList.add('name-notify-show');
         return false;
     } else {
-        inputName.classList.remove('.error');
+        inputName.classList.remove('error');
+        notifyName.classList.remove('name-notify-show');
     }
 
 
     if (!validatePhone(phoneVal)) {
-        console.log('Номер телефона некорректен');
+        notifyPhone.classList.add('phone-notify-show');
         inputPhone.classList.add('error');
         return false;
     } else {
+        notifyPhone.classList.remove('phone-notify-show');
         inputPhone.classList.remove('error');
     }
 
-    /*   if (!validateTextArea(textareaVal)) {
-           console.log('Вы ничего не написали');
-           inputTextArea.classList.add('error');
-           return false;
-       } else {
-           inputTextArea.classList.remove('error');
-       }
-       */
+    if (!validateTextArea(textAreaVal)) {
+        console.log('Вы ничего не написали');
+        inputTextArea.classList.add('error');
+        notifyTextArea.classList.add('notify-text-area-show');
+        return false;
+    } else {
+        inputTextArea.classList.remove('error');
+        notifyTextArea.classList.remove('notify-text-area-show');
+    }
 }
